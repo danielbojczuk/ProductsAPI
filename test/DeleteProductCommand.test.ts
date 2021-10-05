e3b54c3aea3125717fba453192ffb4bc78673235
danielbojczuk <39830414+danielbojczuk@users.noreply.github.com>
2021-10-05 18:11:20 +0200

    Tests improvments

--- test/DeleteProductCommand.test.ts
+++ test/DeleteProductCommand.test.ts
@@ -3,26 +3,27 @@ import { DeleteProductCommand } from '../src/Application/Commands/DeleteProductC
 import { BusinessError } from '../src/Helpers/Exceptions/BusinessError'
 import { ProductRepository } from './MockClasses/ProductRepository'
 
+describe('DeleteCommandHandler', function() {
+  it('Invalid ID', async () => {
 
-it('Delete invalid ID', async () => {
+    let command:DeleteProductCommand ={
+      id: "Invalid ID"
+    }
 
-  let command:DeleteProductCommand ={
-    id: "Invalid ID"
-  }
+    let commandHandler = new DeleteProductCommandHandler(new ProductRepository(),command);
 
-  let commandHandler = new DeleteProductCommandHandler(new ProductRepository(),command);
+    await expect( commandHandler.execute()).rejects.toThrow(BusinessError);
+  });
 
-  await expect( commandHandler.execute()).rejects.toThrow(BusinessError);
-});
+  it('Non-existing ID', async () => {
 
-it('Delete non-existing ID', async () => {
+    let command:DeleteProductCommand ={
+      id: "f63221bd-91e9-4958-ad9f-d254b091c4bf"
+    }
 
-  let command:DeleteProductCommand ={
-    id: "f63221bd-91e9-4958-ad9f-d254b091c4bf"
-  }
+    let commandHandler = new DeleteProductCommandHandler(new ProductRepository(),command);
+    let commandResult = await commandHandler.execute();
 
-  let commandHandler = new DeleteProductCommandHandler(new ProductRepository(),command);
-  let commandResult = await commandHandler.execute();
-
-  expect(commandResult).toBeFalsy();
+    expect(commandResult).toBeFalsy();
+  });
 });
--- test/UpdateProductCommand.test.ts
+++ test/UpdateProductCommand.test.ts
@@ -4,29 +4,31 @@ import { BusinessError } from '../src/Helpers/Exceptions/BusinessError'
 import { ProductRepository } from './MockClasses/ProductRepository'
 
 
-it('Update invalid ID', async () => {
+describe('UpdateProductCommandHandler', function() {
+  it('Invalid ID', async () => {
 
-  let command:UpdateProductCommand ={
-    id: "Invalid ID",
-    description: "Product description",
-    price: 2
-  }
+    let command:UpdateProductCommand ={
+      id: "Invalid ID",
+      description: "Product description",
+      price: 2
+    }
 
-  let commandHandler = new UpdateProductCommandHandler(new ProductRepository(),command);
+    let commandHandler = new UpdateProductCommandHandler(new ProductRepository(),command);
 
-  await expect( commandHandler.execute()).rejects.toThrow(BusinessError);
-});
+    await expect( commandHandler.execute()).rejects.toThrow(BusinessError);
+  });
 
-it('Update non-existing ID', async () => {
+  it('Non-existing ID', async () => {
 
-  let command:UpdateProductCommand ={
-    id: "f63221bd-91e9-4958-ad9f-d254b091c4bf",
-    description: "Product description",
-    price: 2
-  }
+    let command:UpdateProductCommand ={
+      id: "f63221bd-91e9-4958-ad9f-d254b091c4bf",
+      description: "Product description",
+      price: 2
+    }
 
-  let commandHandler = new UpdateProductCommandHandler(new ProductRepository(),command);
-  let commandResult = await commandHandler.execute();
+    let commandHandler = new UpdateProductCommandHandler(new ProductRepository(),command);
+    let commandResult = await commandHandler.execute();
 
-  expect(commandResult).toBeFalsy();
+    expect(commandResult).toBeFalsy();
+  });
 });
